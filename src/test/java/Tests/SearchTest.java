5a74c0c255f3ef7d9464d9c187be227437d48468
Kr0lik <kr0lik3772@gmail.com>
2023-10-26 18:38:18 +0200

    Changes to browser provider

--- src/test/java/Tests/SearchTest.java
+++ src/test/java/Tests/SearchTest.java
@@ -4,9 +4,16 @@ package Tests;/*
 */
 
 import Commons.BaseTest;
+import Commons.BrowserProvider;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SearchTest extends BaseTest {
+
+    WebDriver driver;
 
     @Test
     void test(){
